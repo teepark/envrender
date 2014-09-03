0f5ad8863171eeec67e85cb8ada71f4cda0e196b
Travis J Parker <travis.parker@gmail.com>
2014-09-03 16:26:09 -0700

    comment

--- main.go
+++ main.go
@@ -9,24 +9,30 @@ import (
 
 func main() {
 	for _, path := range os.Args[1:] {
+
+		/* parse the file as a template */
 		t, err := template.ParseFiles(path)
 		if err != nil {
 			fmt.Println(path, "parsing:", err)
 			os.Exit(1)
 		}
 
+		/* empty and then open that same file for writing */
 		wr, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC, 0)
 		if err != nil {
 			fmt.Println(path, "writing:", err)
 			os.Exit(1)
 		}
 
+		/* execute the template with environment vars,
+		 * write the output back to the original file */
 		err = t.Execute(wr, environ())
 		if err != nil {
 			fmt.Println(path, "executing:", err)
 			os.Exit(1)
 		}
 
+		/* close the file writer */
 		err = wr.Close()
 		if err != nil {
 			fmt.Println(path, "closing:", err)
